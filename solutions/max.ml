de03be58efd5b38f49fc642e5b6f75b51ad3267b
Mário Pereira <mjp.pereira@fct.unl.pt>
2024-09-07 15:43:09 +0100

    Solutions: added [max.ml] file

---
+++ solutions/max.ml
@@ -0,0 +1,24 @@
+module type PRE_ORD = sig
+  type t
+
+  (*@ function le: t -> t -> bool *)
+
+  (*@ axiom reflexive : forall x. le x x *)
+  (*@ axiom total     : forall x y. le x y \/ le y x *)
+  (*@ axiom transitive: forall x y z. le x y -> le y z -> le x z *)
+
+  val leq : t -> t -> bool
+  (*@ b = leq x y
+        ensures b <-> le x y *)
+end
+
+module Max (E : PRE_ORD) = struct
+
+  let max x y =
+    if E.leq x y then y
+    else x
+  (*@ r = max x y
+        ensures E.le x r /\ E.le y r
+        ensures r = x \/ r = y *)
+
+end
